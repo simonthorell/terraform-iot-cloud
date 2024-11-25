e43c1c6720d9bf0188472662318b7813ab46edec
Simon Thorell <kontakt@simonthorell.se>
2024-11-25 21:13:08 +0100

    Add device table in DynamoDB

--- terraform/dynamodb/main.tf
+++ terraform/dynamodb/main.tf
@@ -3,12 +3,12 @@
 #===================================================================
 resource "aws_dynamodb_table" "iot_data" {
   name           = "iot_data"
-  hash_key       = "device_id"
-  range_key      = "timestamp"
+  hash_key       = "device_id" # Primary key
+  range_key      = "timestamp" # Sort key
 
   attribute {
     name = "device_id"
-    type = "S"
+    type = "S" # String type
   }
 
   attribute {
@@ -19,6 +19,37 @@ resource "aws_dynamodb_table" "iot_data" {
   billing_mode = "PROVISIONED"
 
   # Specify provisioned read and write capacities
+  read_capacity  = 5
+  write_capacity = 5
+
+  # Enable a DynamoDB stream to capture data changes
+  stream_enabled   = true
+  stream_view_type = "NEW_IMAGE"
+}
+
+resource "aws_dynamodb_table" "devices" {
+  name           = "devices"
+  hash_key       = "device_id" # Primary key
+  range_key      = "owner"     # Sort key
+
+  attribute {
+    name = "device_id"
+    type = "S" # String type
+  }
+
+  attribute {
+    name = "owner"
+    type = "S" # String type for required "owner" attribute
+  }
+
+  # Optional attribute
+  # attribute {
+  #   name = "status"
+  #   type = "S" # String type for optional "status" attribute
+  # }
+
+  billing_mode = "PROVISIONED"
+
   read_capacity  = 5
   write_capacity = 5
 
--- terraform/dynamodb/outputs.tf
+++ terraform/dynamodb/outputs.tf
@@ -8,3 +8,11 @@ output "table_name" {
 output "table_arn" {
   value = aws_dynamodb_table.iot_data.arn
 }
+
+output "devices_table_name" {
+  value = aws_dynamodb_table.devices.name
+}
+
+output "devices_table_arn" {
+  value = aws_dynamodb_table.devices.arn
+}
