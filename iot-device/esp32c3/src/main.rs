65879b36261331be72454d94022c58cb73931dd7
Simon Thorell <kontakt@simonthorell.se>
2024-11-30 17:30:19 +0100

    Add Rust Wifi module to ESP32

--- iot-device/esp32c3/src/main.rs
+++ iot-device/esp32c3/src/main.rs
@@ -1,29 +1,26 @@
+// Modules
+mod wifi;
+
 // Namespaces
 use esp_idf_svc::log::EspLogger;
 
-// Specify Global Constants
-const WIFI_SSID: &str = env!("WIFI_SSID");
-const WIFI_PASSWORD: &str = env!("WIFI_PASSWORD");
-
+// TODO: Define in mqtt.rs module
 const MQTT_SUB_TOPIC: &str = env!("MQTT_SUB_TOPIC");
 const MQTT_PUB_TOPIC: &str = env!("MQTT_PUB_TOPIC");
 const MQTT_PORT: &str = env!("MQTT_PORT");
 const MQTT_ENDPOINT: &str = include_str!("/certs/iot_endpoint.txt");
 
+// TODO: Define in mtls.rs module
 const AWS_CERT_CA: &str = include_str!("/certs/root_ca.pem");
 const AWS_CERT_CRT: &str = include_str!("/certs/iot_cert.pem");
 const AWS_CERT_PRIVATE: &str = include_str!("/certs/iot_private_key.pem");
 
-fn main() {
+// fn main() {
+fn main() -> Result<(), anyhow::Error> {
     esp_idf_svc::sys::link_patches(); // Required to apply ESP-IDF patches
 
     // Initialize logger
     EspLogger::initialize_default();
-
-    log::info!("Hello, world!");
-
-    log::info!("WIFI_SSID: {}", WIFI_SSID);
-    log::info!("WIFI_PASSWORD: {}", WIFI_PASSWORD);
 
     log::info!("MQTT_SUB_TOPIC: {}", MQTT_SUB_TOPIC);
     log::info!("MQTT_PUB_TOPIC: {}", MQTT_PUB_TOPIC);
@@ -33,4 +30,10 @@ fn main() {
     log::info!("AWS Cert CA: {}", AWS_CERT_CA);
     log::info!("AWS Cert CRT: {}", AWS_CERT_CRT);
     log::info!("AWS Cert Private: {}", AWS_CERT_PRIVATE);
+
+    // Use the `connect_to_wifi` function from the `wifi` module
+    log::info!("Connecting to wifi...");
+    wifi::connect_to_wifi()?;
+
+    Ok(()) // Return `Ok` to indicate success
 }
---
+++ iot-device/esp32c3/src/wifi.rs
@@ -0,0 +1,49 @@
+use core::convert::TryInto;
+
+use embedded_svc::wifi::{AuthMethod, ClientConfiguration, Configuration};
+use esp_idf_svc::hal::prelude::Peripherals;
+use esp_idf_svc::wifi::{BlockingWifi, EspWifi};
+use esp_idf_svc::{eventloop::EspSystemEventLoop, nvs::EspDefaultNvsPartition};
+
+use log::info;
+
+// Use the environment variables for Wi-Fi SSID and password
+const SSID: &str = env!("WIFI_SSID");
+const PASSWORD: &str = env!("WIFI_PASSWORD");
+
+// Function to set up and connect to Wi-Fi
+pub fn connect_to_wifi() -> anyhow::Result<()> {
+    let peripherals = Peripherals::take()?;
+    let sys_loop = EspSystemEventLoop::take()?;
+    let nvs = EspDefaultNvsPartition::take()?;
+
+    let mut wifi = BlockingWifi::wrap(
+        EspWifi::new(peripherals.modem, sys_loop.clone(), Some(nvs))?,
+        sys_loop,
+    )?;
+
+    let wifi_configuration: Configuration = Configuration::Client(ClientConfiguration {
+        ssid: SSID.try_into().unwrap(),
+        bssid: None,
+        auth_method: AuthMethod::WPA2Personal,
+        password: PASSWORD.try_into().unwrap(),
+        channel: None,
+        ..Default::default()
+    });
+
+    wifi.set_configuration(&wifi_configuration)?;
+
+    wifi.start()?;
+    info!("Wi-Fi started");
+
+    wifi.connect()?;
+    info!("Wi-Fi connected");
+
+    wifi.wait_netif_up()?;
+    info!("Wi-Fi netif up");
+
+    let ip_info = wifi.wifi().sta_netif().get_ip_info()?;
+    info!("Wi-Fi IP info: {:?}", ip_info);
+
+    Ok(())
+}
